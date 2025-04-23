e159e1e174bead13bd431443001ebac79643d051
AmyMell <melloncraft1@gmail.com>
2025-04-23 15:05:30 +0200

    first

---
+++ Oppgave 1/src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const guessNumber = ref<number | null>(null);
+const guessNumberInput = ref<number | null>(null);
+const secretNumber = ref<number>(0);
+
+const Attempts = ref<number>(0);
+
+function start() {
+  guessNumber.value = null;
+  guessNumberInput.value = null;
+  secretNumber.value = Math.ceil(Math.random() * 100);
+  Attempts.value = 0
+}
+
+function svar() {
+  if (guessNumberInput.value !== null) {
+    guessNumber.value = guessNumberInput.value;
+    Attempts.value = Attempts.value + 1
+  }
+}
+
+
+start();
+</script>
+
+<template>
+  <div>
+    <h2>Tipp-tallet-spillet</h2>
+    
+    <p v-if="guessNumber === null"></p>
+    <p v-else>Du tippet {{ guessNumber }}.<br/> </p>
+   
+
+    <p v-if="guessNumber === null">Gjett et number </p>
+      
+   
+    <p v-else-if="guessNumber === secretNumber">Rikitig svar</p>
+    <p v-else-if="guessNumber < secretNumber">Du har tippet for lavt.</p>
+    <p v-else-if="guessNumber > secretNumber">Du har tippet for høyt.</p>
+    <p v-else>ikke en number</p>
+    <p>Du har gjettet {{ Attempts }} ganger</p>
+    <input type="text" v-model.number="guessNumberInput" />
+    <button @click="svar">Tipp</button>
+    <button @click="start">Start på nytt</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #6d6d6d;
+}
+</style>
---
+++ Oppgave 2/src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <div id="trafficLight" @click="changeLight">
+      <div class="light" :style="{ backgroundColor: currentLightColor === 'red' ? 'red' : 'gray' }"></div>
+      <div class="light" :style="{ backgroundColor: currentLightColor === 'yellow' ? 'yellow' : 'gray' }"></div>
+      <div class="light" :style="{ backgroundColor: currentLightColor === 'green' ? 'green' : 'gray' }"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const currentLightColor = ref<'red' | 'yellow' | 'green'>('green');
+    const changeLight = () => {
+      if (currentLightColor.value === 'green') {
+        currentLightColor.value = 'yellow';
+      } else if (currentLightColor.value === 'yellow') {
+        currentLightColor.value = 'red';
+      } else if (currentLightColor.value === 'red') {
+        currentLightColor.value = 'green';
+      }
+    };
+
+    return {
+      currentLightColor,
+      changeLight,
+    };
+  },
+});
+</script>
+
+<style>
+#trafficLight {
+  display: flex;
+  flex-direction: column;
+  width: 3rem;
+  height: 10rem;
+  background-color: black;
+  padding: 1rem;
+  gap: 1rem;
+  cursor: pointer; 
+}
+
+div.light {
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+}
+</style>
